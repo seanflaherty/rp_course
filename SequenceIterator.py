77fd50b8c431d986e5a10a474d81fd73b05a20b8
Sean Flaherty <sean.flaherty@gmail.com>
2025-05-12 11:30:51 -0400

    update with import of Iterator from collections.abc.

--- SequenceIterator.py
+++ SequenceIterator.py
@@ -1,10 +1,13 @@
+from collections.abc import Iterator
+
 class SequenceIterator:
     def __init__(self, sequence):
         self._index = 0
         self._sequence = sequence
-
-    def __iter__(self):
-        return self
+    
+    # not needed with Iterator import
+    # def __iter__(self):
+    #     return self
     
     def __next__(self):
         if self._index < len(self._sequence):
