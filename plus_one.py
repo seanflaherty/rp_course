117143b1f16a129b19e7964bc24804607061fe25
Sean Flaherty <sean.flaherty@gmail.com>
2025-05-19 08:49:36 -0400

    Add more week three code.

---
+++ plus_one.py
@@ -0,0 +1,6 @@
+seasons = ["Spring", "Summer", "Fall", "Winter"]
+count = 1
+
+for season in seasons:
+    print(count, season)
+    count +=1
---
+++ range_len.py
@@ -0,0 +1,4 @@
+seasons = ["Spring", "Summer", "Fall", "Winter"]
+
+for count in range(len(seasons)):
+    print(count + 1, seasons[count])
